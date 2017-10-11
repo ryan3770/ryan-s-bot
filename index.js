8204381aaa8c9ee06083275966f82e6a0aaf1852
ryan3770 <aryan05jan@gmail.com>
2017-10-11 18:14:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+onst Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.login(process.env.TOKEN);
+
+client.on("ready", () => {
+console.log("I am ready!");
+});
+
+client.on("message", (message)=> {
+if (message.content.startsWith("ping")) {
+message.channel.send("pong!");
+}
+});
